88bd0c112c6d5f5301e3ba2a84164531823efe08
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-03-17 03:00:22 +0530

    feat: added categories & featured products

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,16 @@
 import "./Home.css";
-import { HeroBanner } from "../../components";
+import {
+  HeroBanner,
+  ExploreCategories,
+  FeaturedProducts,
+} from "../../components";
+
 export const Home = () => {
   return (
     <main className="main-wrapper">
       <HeroBanner />
+      <ExploreCategories />
+      <FeaturedProducts />
     </main>
   );
 };
