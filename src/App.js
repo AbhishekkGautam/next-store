17aa2c2ba38589af415926007fdc467356a3a8f7
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-03-20 23:28:24 +0530

    added user info & protected routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import Mockman from "mockman-js";
 import { Navbar } from "./components";
-import { Home, Products, Wishlist, Cart, Login } from "./pages";
+import { Home, Products, Wishlist, Cart, Login, Signup } from "./pages";
+import { useAuth } from "./context/AuthContext";
 function App() {
+  const {
+    state: { isLoggedIn },
+  } = useAuth();
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
+        {isLoggedIn && <Route path="/wishlist" element={<Wishlist />} />}
+        {isLoggedIn && <Route path="/cart" element={<Cart />} />}
+        {!isLoggedIn && <Route path="/signup" element={<Signup />} />}
+        {!isLoggedIn && <Route path="/login" element={<Login />} />}
         <Route path="/mock" element={<Mockman />} />
+        <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </div>
   );
--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 export const HeroBanner = () => {
   return (
     <section>
-      <div class="hero">
-        <div class="hero-img">
-          <img class="img-responsive" src={HeroImage} alt="hero-img" />
+      <div className="hero">
+        <div className="hero-img">
+          <img className="img-responsive" src={HeroImage} alt="hero-img" />
         </div>
-        <div class="hero-content flex-center flex-col text-center">
-          <div class="">
-            <h1 class="hero-title">UPTO 80% OFF</h1>
+        <div className="hero-content flex-center flex-col text-center">
+          <div className="">
+            <h1 className="hero-title">UPTO 80% OFF</h1>
           </div>
-          <div class="">
-            <p class="hero-description">
+          <div className="">
+            <p className="hero-description">
               Get your next smart wearables at the best price.
             </p>
           </div>
-          <div class="hero-btn">
-            <Link class="btn btn-primary" to="/products">
+          <div className="hero-btn">
+            <Link className="btn btn-primary" to="/products">
               Shop Now
             </Link>
           </div>
